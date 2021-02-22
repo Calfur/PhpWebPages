e7d20dfe1f9959a9c62932e53beb7d501da0ad1b
Calfur <robin.nater@hotmail.com>
2021-02-22 15:43:42 +0100

    color update

--- Car/js/table.js
+++ Car/js/table.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
    $('.modal').modal();
+
+   $.getJSON("car.json", function (response) {
+         console.log(response);
+         var template = $("car-template").html();
+         var html = Mustache.render(template, response);
+         $("tbody").html(html);
+      }
+   );
+
    $(".edit-data").click(function (e) { 
       e.preventDefault();
       $("#modal-title").html("Edit: " + $(this).parent().attr("data-dataset"))
