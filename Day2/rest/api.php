09957244f7210c4e19faa8fcebd3203bd8d2ff0a
Calfur <robin.nater@hotmail.com>
2021-02-23 16:28:43 +0100

    php test

---
+++ Day2/rest/api.php
@@ -0,0 +1,12 @@
+<?php
+   $myarr = array();
+   $myarr['data'][0] = array();
+   $myarr['data'][1] = array();
+   $myarr['data'][0]['name'] = "Peter";
+   $myarr['data'][0]['id'] = 1;
+   echo json_encode($myarr);
+
+
+
+
+?>
